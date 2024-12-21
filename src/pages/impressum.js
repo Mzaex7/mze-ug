d2427588268c81d2412f9fea486f58ecc1fbbab3
Mzaex <147636689+Mzaex@users.noreply.github.com>
2024-12-21 23:50:02 +0100

    Initial commit

---
+++ src/pages/impressum.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Impressum() {
+  return (
+    <Layout>
+      <div style={{ margin: '50px auto', maxWidth: '1200px', fontSize: '20px' }}>
+        <h1 style={{ textAlign: 'center', fontSize: '40px', fontWeight: 'bold' }}>Impressum</h1>
+        <p style={{ textAlign: 'center', margin: 0 }}>
+          Die folgenden Angaben gelten für die Geschäftsaktivitäten der Mark Zeitler Engineering UG
+          (haftungsbeschränkt):
+        </p>
+        <hr style={{ margin: '50px 0' }} />
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '50px' }}>
+          <div>
+            <h2 style={{ fontSize: '28px', fontWeight: 'bold' }}>Mark Zeitler Engineering UG</h2>
+            <p style={{ margin: 0 }}>Schumannstr. 15</p>
+            <p style={{ margin: 0 }}>16359 Biesenthal</p>
+            <p style={{ margin: 0 }}>
+              <a href="mailto:info@markzeitler.de">info@markzeitler.de</a>
+            </p>
+          </div>
+          <div>
+            <h2 style={{ fontSize: '28px', fontWeight: 'bold' }}>Kontakt:</h2>
+            <p style={{ margin: 0 }}>Telefon: +49 30 12345678</p>
+          </div>
+          <div>
+            <h2 style={{ fontSize: '28px', fontWeight: 'bold' }}>Geschäftsführer:</h2>
+            <p style={{ margin: 0 }}>Mark Zeitler</p>
+          </div>
+          <div>
+            <h2 style={{ fontSize: '28px', fontWeight: 'bold' }}>Sitz der Gesellschaft:</h2>
+            <p style={{ margin: 0 }}>Biesenthal</p>
+            <p style={{ margin: 0 }}>Amtsgericht Frankfurt (Oder) HRB 12527 FF</p>
+          </div>
+          <div>
+            <h2 style={{ fontSize: '28px', fontWeight: 'bold' }}>Umsatzsteuer-ID:</h2>
+            <p style={{ margin: 0 }}>DE123456789</p>
+          </div>
+          <div>
+            <h2 style={{ fontSize: '28px', fontWeight: 'bold' }}>Registrierungsnummer EAR:</h2>
+            <p style={{ margin: 0 }}>WEEE-Reg.-Nr. DE 987654321</p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import styles from './index.module.css';
+import Button from './button';
+
+export default function Home(): JSX.Element {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+
+  const scrollToFirstText = () => {
+    document.getElementById('first-text').scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <Layout
+      title={siteConfig.title}
+      description={siteConfig.tagline}
+      keywords={siteConfig.customFields.keywords}
+      wrapperClassName='es-footer-white'
+    >
+      <div className={styles.hero}>
+        <h1 className={`${styles.title} padding-left-180`}>
+          Mark Zeitler
+          <br />
+          Engineering UG
+        </h1>
+        <Button onClick={scrollToFirstText} /> {/* Übergebe die scrollToFirstText-Funktion als onClick-Prop */}
+        <p className={`${styles.subtitle} padding-left-180`} id="first-text">
+          Die Mark Zeitler Engineering UG (haftungsbeschränkt) ist ein Service- und Beratungsunternehmen
+          für Engineering-Leistungen der Elektrotechnik / Leittechnik
+        </p>
+        <p className={`${styles.tagline} padding-left-180`}>
+          erfahren, zuverlässig, flexibel und effizient
+        </p>
+        <p className={`${styles.services} padding-left-180`}>
+          PROGRAMMIERUNG * REPARATUR * WARTUNG * INBETRIEBNAHME * ANLAGENOPTIMIERUNG
+        </p>
+        <p className={`${styles.description} padding-left-180`}>
+          Ich berate und unterstütze herstellerunabhängig meine Kunden bei der Planung und Umsetzung von
+          Elektro- und MSR-technischen Aufgabenstellungen.
+        </p>
+        <p className={`${styles.description} padding-left-180`}>
+          Im Vordergrund steht die durchgängige Unterstützung in allen Phasen eines Projektes.
+        </p>
+        <h2 className={`${styles.sectionTitle} padding-left-180`}>Die Betätigungsfelder sind insbesondere:</h2>
+        <ul className={`${styles.list} padding-left-180`}>
+          <li>Sicherheitstechnische Prüfungen und Brennereinstellungen von Feuerungsanlagen (thermische Abgasreinigungen, Dampf- und Heißwassererzeuger und Sonderofenbau)</li>
+          <li>Störungsbeseitigung in Elektro- und MSR-Anlagen (bis 1kV), Feuerungsanlagen</li>
+          <li>Projektierung, Programmierung und Inbetriebnahme von:</li>
+          <ul>
+            <li>S5- und S7 Steuerungen mit Profibus- und Ethernetanbindung (Thermische Abgasreinigungsanlagen / Feuerungsanlagen in der chemischen Industrie; Produktions- und Fördertechnikanlagen in der Automobilindustrie; Prüföfen/Sonderofenbau)</li>
+            <li>Hima-Steuerungen mittels Elop II NT/XP und Elop II Factory</li>
+            <li>Reglern diverser Hersteller (Siemens, H&B, ABB, PMA, Phillips, K&P, Mytec und Lamtec)</li>
+            <li>Schneider Steuerungen (Concept)</li>
+            <li>Visualisierungssystemen Intouch (Wonderware), WINCC (Siemens), Wizcon Supervisor (Emation)</li>
+            <li>Siemens Operator- / Touch- und Multipanels mit Protool oder Wincc Flex.</li>
+            <li>Meßdatenerfassungssystem / Visualisierung – PMA’s MSI-WPCI</li>
+            <li>Frequenzumrichtern div. Hersteller</li>
+          </ul>
+          <li>Fernwartung (z.B. pcAnywhere, VNC und Teleservice)</li>
+          <li>Projektierung, Montage und Inbetriebnahme von Automatisierungsanlagen</li>
+          <li>Aufbau, Installation, Sicherung und Konfiguration von PC-Systemen inkl. Netzwerk</li>
+        </ul>
+      </div>
+    </Layout>
+  );
+}
